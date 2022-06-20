fa79836c1615e70565b26f26857602242019b87f
Over-Infinity <67686187+over-infinity@users.noreply.github.com>
2022-06-20 15:27:20 +0430

    Update task.h

--- core/task.h
+++ core/task.h
@@ -1,5 +1,31 @@
 struct Task{
   
+  struct promise_type{
+    
+    ~promise_type(){}
+    
+    Task get_return_object(){
+      return Task(std::coroutine_handler<promise_type>::from_promise(*this);
+    }
+    
+    void return_void(){
+      ;
+    }
+    
+    std::suspend_never initial_suspend(){
+      return {};
+    }
+    
+    std::suspend_always final_suspend(){
+      return {};
+    }
+    
+    void unhandled_exception(){
+     ; 
+    }
+    
+  };
+  
   
   
 };
